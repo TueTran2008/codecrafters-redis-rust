0f0792a2f2375937db62e73b8da0314f0d722cdb
Darwin_Tran <Darwin_Tran@vn.gemteks.com>
2024-09-10 21:05:18 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
-#![allow(unused_imports)]
-use std::net::TcpListener;
+#![allow(unused_imports, unused_variables)]
+use anyhow::Ok;
+//#[tokio::main]
+//use mini_redis::{client, Result};
+//use std::{io, net::TcpListener};
+use tokio::{
+    io::AsyncWriteExt,
+    net::{TcpListener, TcpStream},
+};
 
-fn main() {
+async fn process_socket(mut socket: TcpStream) {
+    let pong = "+PONG\r\n";
+    //println!("Accpet a connection: {:?}", socket);
+    let _ = socket.write_all(pong.as_bytes()).await;
+}
+
+#[tokio::main()]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    // Uncomment this block to pass the first stage
-    //
-    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
-        }
+    let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
+    loop {
+        let (socket, _) = listener.accept().await.unwrap();
+        process_socket(socket).await;
     }
 }
